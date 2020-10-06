00c2127a58cbea387efe94024ce920cbf1246d12
Hugo Silva <hugsil@amazon.com>
2020-10-05 22:39:43 -0300

    New changes

---
+++ src/test/kotlin/com/br/ecommerce/store/services/TableauServiceTest.kt
@@ -0,0 +1,29 @@
+package com.br.ecommerce.store.services
+
+import org.junit.jupiter.api.Test
+
+class TableauServiceTest {
+    @Test
+    fun `given valid credentials should return a response payload object with success`() {
+
+    }
+    @Test
+    fun `given invalid credentials should return an exception ExempleAuth`() {
+
+    }
+    @Test
+    fun `given invalid payload should return an exception ExempleDesearilaztion`() {
+
+    }
+    @Test
+    fun `given valid token should return a response a csv format with success`() {
+
+    }
+    @Test
+    fun `given invalid token should return an exception ExempleAuth`() {
+
+    }
+    @Test
+    fun `given invalid payload on CSV endpoint should return an exception ExempleDesearilaztion`() {
+    }
+}
