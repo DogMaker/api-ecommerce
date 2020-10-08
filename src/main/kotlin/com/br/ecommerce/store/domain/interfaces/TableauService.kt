8ddc0f238f86a70d55e53b57a97c88f36f40814f
Hugo Silva <hugsil@amazon.com>
2020-10-08 15:46:58 -0300

    src/main/kotlin/com/br/ecommerce/store/domain/interfaces/TableauService.kt

--- src/main/kotlin/com/br/ecommerce/store/domain/interfaces/TableauService.kt
+++ src/main/kotlin/com/br/ecommerce/store/domain/interfaces/TableauService.kt
@@ -1,11 +1,12 @@
-package com.br.ecommerce.store.model.interfaces
+package com.br.ecommerce.store.domain.interfaces
 
 import com.br.ecommerce.store.model.entities.LoginResponseTableau
 import com.br.ecommerce.store.model.entities.Parameters
+import org.springframework.http.ResponseEntity
 
 interface TableauService{
 
-     fun authentication(): LoginResponseTableau
+     fun authentication(): LoginResponseTableau?
 
      fun retriveCsvData(token: String?, params : Parameters): String
 }
