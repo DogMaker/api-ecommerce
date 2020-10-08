a11a0133c37feaa3f5b7b8460c78c24e240e4068
Hugo Silva <hugsil@amazon.com>
2020-10-08 15:46:31 -0300

    Changed the restemplated request from the object to exchange

--- src/main/kotlin/com/br/ecommerce/store/services/TableauServiceImpl.kt
+++ src/main/kotlin/com/br/ecommerce/store/services/TableauServiceImpl.kt
@@ -1,27 +1,29 @@
-package com.br.ecommerce.store.service
+package com.br.ecommerce.store.services
 
 import com.br.ecommerce.store.domain.exceptions.AutenticationTableauException
 import com.br.ecommerce.store.domain.exceptions.ConectionTableauException
+import com.br.ecommerce.store.domain.interfaces.TableauService
 import com.br.ecommerce.store.helpers.SecurityHandler
 import com.br.ecommerce.store.model.entities.*
-import com.br.ecommerce.store.model.interfaces.TableauService
-import org.springframework.context.annotation.Configuration
 import org.springframework.http.HttpEntity
 import org.springframework.http.HttpHeaders
-import org.springframework.http.HttpMethod
+import org.springframework.http.HttpMethod.GET
+import org.springframework.http.HttpMethod.POST
 import org.springframework.stereotype.Service
 import org.springframework.web.client.HttpStatusCodeException
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.util.UriComponentsBuilder
 import java.net.UnknownHostException
 
+
 @Service
-class TableauServiceImpl : TableauService {
-    private val uri = "https://run.mocky.io/v3/2220fc04-ac9c-4123-b4e1-b3b628cf107d"
+class TableauServiceImpl(val restTemplate: RestTemplate = RestTemplate()) : TableauService {
 
-    private val restTemplate = RestTemplate()
+    private val uri = "https://run.mocky.io/v3"
 
-    override fun authentication(): LoginResponseTableau {
+    override fun authentication(): LoginResponseTableau? {
+        val path = "/a0d4d954-5e06-42df-97f1-392cc723c723"
+
         SecurityHandler.trustSelfSignedSSL()
 
         val loginTableau = LoginTableau(Credentials("hugsil", "", ContentUrl("")))
@@ -32,23 +34,25 @@ class TableauServiceImpl : TableauService {
         val request: HttpEntity<LoginTableau> = HttpEntity(loginTableau, headers)
 
         return try {
-            restTemplate.postForObject(uri, request, LoginResponseTableau::class.java)!!
+            val response = restTemplate.exchange("$uri$path", POST, request, LoginResponseTableau::class.java)
+            response.body
         } catch (e: HttpStatusCodeException) {
             throw AutenticationTableauException()
         } catch (e: UnknownHostException){
             throw ConectionTableauException()
         }
     }
-    override fun retriveCsvData(token: String?, params : Parameters): String {
 
-        val uri2 = "https://skynet.db.amazon.com/api/3.1/sites/a946d998-2ead-4894-bb50-1054a91dcab3/views/79db6728-eaa6-4b98-9897-6898d70b6c82/data"
+    override fun retriveCsvData(token: String?, params: Parameters): String {
+        val path = "/2220fc04-ac9c-4123-b4e1-b3b628cf107d"
 
         SecurityHandler.trustSelfSignedSSL()
+
         val headers = HttpHeaders()
         headers.add("accept", "application/json")
         headers.add("X-Tableau-Auth", token)
 
-        val builder = UriComponentsBuilder.fromHttpUrl(uri2)
+        val uriBuilder = UriComponentsBuilder.fromHttpUrl("$uri$path")
                 .queryParam("vf_locale", params.locale)
                 .queryParam("vf_Start Date B", params.startDateB)
                 .queryParam("vf_End Date B", params.endDateB)
@@ -57,14 +61,17 @@ class TableauServiceImpl : TableauService {
                 .queryParam("vf_Choose a Metric", params.metric)
 
         val header: HttpEntity<String> = HttpEntity(headers)
-        val response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, header, String::class.java)
-
-        return response.toString()
+        return try {
+            val response =  restTemplate.exchange(uriBuilder.toUriString(), GET, header, String::class.java)
+            response.body.toString()
+        } catch (e: UnknownHostException){
+            throw ConectionTableauException()
+        }
     }
 }
 
 fun main() {
-    val authToken = TableauServiceImpl().authentication().credentials?.token
-    val parameters = Parameters("pt_BR,en_US","2020-06-25","2020-06-26","2020-06-26","2020-06-26","GSR")
+    val authToken = TableauServiceImpl().authentication()?.credentials?.token
+    val parameters = Parameters("pt_BR,en_US", "2020-06-25", "2020-06-26", "2020-06-26", "2020-06-26", "GSR")
     println(TableauServiceImpl().retriveCsvData(authToken, parameters))
 }
