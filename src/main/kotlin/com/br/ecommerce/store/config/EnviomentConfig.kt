0e8936206b1605e1726d4b98afd31be611516dd5
Hugo Silva <hugsil@amazon.com>
2020-10-13 20:04:12 -0300

    Added a fallback to enviconfig

---
+++ src/main/kotlin/com/br/ecommerce/store/config/EnviomentConfig.kt
@@ -0,0 +1,9 @@
+package com.br.ecommerce.store.config
+
+class EnviomentConfig{
+    val tableauUri = "https://run.mocky.io/v3"
+    val tableauPathAuth = "/b153d8d9-c498-4f0d-b1e9-f51558745620"
+    val tableauUser = "hugsil"
+    val tableauPass = "123pass"
+    val tableauPathCsv = "/767607f7-bcb0-4fc4-bb2d-2165c1a9858a"
+}
