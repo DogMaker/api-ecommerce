122765ff43d3772d97817e522b9f96049dd4f4b2
Hugo Silva <hugsil@amazon.com>
2020-10-13 20:06:53 -0300

    Added a new endpoit to retrive data

--- src/main/kotlin/com/br/ecommerce/store/domain/interfaces/TableauService.kt
+++ src/main/kotlin/com/br/ecommerce/store/domain/interfaces/TableauService.kt
@@ -1,14 +1,16 @@
 package com.br.ecommerce.store.domain.interfaces
 
+import com.br.ecommerce.store.controller.ParametersRequestTableau
 import com.br.ecommerce.store.model.entities.LoginResponseTableau
 import com.br.ecommerce.store.model.entities.Parameters
-import org.springframework.http.ResponseEntity
 
 interface TableauService{
 
      fun authentication(): LoginResponseTableau?
 
      fun retriveCsvData(token: String?, params : Parameters): String
+
+     fun getData(params: ParametersRequestTableau): String
 }
 
 
